00eef74ca48b3124b6f15a7f8e5fda1af031e44f
jtskidmore <johntoddskidmore@gmail.com>
2020-07-21 15:21:53 -0500

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,36 @@
+
+
 class Instructor
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.pass_student(student, test_name)
+    test = BoatingTest.all.find {|test| test.student == student && test.test_name == test_name}
+    if test != nil
+      test.status = "passed"
+      test
+    else
+      BoatingTest.new(student, test_name, "passed", self)
+    end
+  end
+
+  def self.fail_student(student, test_name)
+    test = BoatingTest.all.find {|test| test.student == student && test.test_name == test_name}
+    if test != nil
+      test.status = "failed"
+      test
+    else
+      BoatingTest.new(student, test_name, "failed", self)
+    end
+  end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,32 @@
+
+
 class Student
+
+  attr_reader :first_name
+
+  @@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(test_name, status, instructor)
+    BoatingTest.new(self, test_name, status, instructor)
+  end
+
+  def self.find_student(first_name)
+    self.all.find {|student| student.first_name == first_name}
+  end
+
+  def grade_percentage
+    students_tests = BoatingTest.all.select {|test| test.student == self}
+    students_passed_tests = students_tests.select {|test| test.status == "passed"}
+    students_passed_tests.length.to_f/students_tests.length.to_f*100.round(2)
+  end
 
 end
